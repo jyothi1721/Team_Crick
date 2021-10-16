5dceec68a8228546de7a6bff5e4ef72fdc89e041
Jyothi Guruprasad <88387089+jyothi1721@users.noreply.github.com>
2021-10-16 18:34:44 +0530

    Update Crick_hackbio.sh

--- Crick_hackbio.sh
+++ Crick_hackbio.sh
@@ -2,7 +2,7 @@
 
 Packages should be equal to or above the following: Python 3.8.10, BASH 5.0.17(1), R 3.6 (DescTools package installed)
 
-Run the command: "$./Crick_hackbio.sh" in the terminal
+Run the command: "$./Crick_hackbio.sh" in the terminal'
 
 #Downloading repo to local machine
 git clone https://github.com/jyothi1721/Team_Crick.git
