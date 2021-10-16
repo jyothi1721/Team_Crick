2299f3338b779e46339cb1d265d7229318488ad9
Jyothi Guruprasad <88387089+jyothi1721@users.noreply.github.com>
2021-10-17 00:47:18 +0530

    Update Crick_hackbio.sh

--- Crick_hackbio.sh
+++ Crick_hackbio.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
-REPO = https://github.com/jyothi1721/Team_Crick
-OUTPUT_FILE = Crick_hackbio.csv
+#cloning github repository
+git clone https://github.com/jyothi1721/Team_Crick
 
-my_value=$(python Josphat.py)
-echo $my_value >> Crick_hackbio.csv
+#create a csv file containing team details
+echo 'Team CrickDARWIN' >>Team_Crick.csv;
+echo ' ' >>Team_Crick.csv;
+echo ' ' >>Team_Crick.csv;
+echo 'Name, Email, Slack_Username, Biostack' >>Team_Crick.csv;
 
-my_value_1=$(python Nourhan.py)
-echo $my_value_1 > Crick_hackbio.csv
 
-Rscript KV.R >> Crick_hackbio.csv
+#generating the csv files
 
-Rscript Pratiksha.R >> Crick_hackbio.csv
+for FILE1 in Team_Crick/Jyothi*;do gcc $FILE1 -o $FILE1.txt; ./$FILE1.txt >>Team_Crick.csv; done;
 
-sed -i -e '1i"Name","Email","slack","biostack"' Crick_hackbio.csv
+for FILE2 in Team_Crick/Josphat*;do python3 $FILE2 >>Team_Crick.csv; done;
 
-gcc Jyothi.C > Crick_hackbio.csv
+for FILE3 in Team_Crick/KV*;do Rscript $FILE3 >>Team_Crick.csv; done;
 
+for FILE4 in Team_Crick/Nourhan*;do python3 $FILE4 >>Team_Crick.csv; done;
+
+for FILE5 in Team_Crick/Pratiksha*;do Rscript $FILE5 >>Team_Crick.csv; done;
+
+for FILE6 in Team_Crick/Samgle*;do node $FILE6; cat Details.txt >>Team_Crick.csv; done;
+
+for FILE7 in Team_Crick/Yash*;do clang -o $FILE7 >>Team_Crick.csv; done;
