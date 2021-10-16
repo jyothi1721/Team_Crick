097c745a7e94fc3176303cb9a34c27e15dd118e0
Jyothi Guruprasad <88387089+jyothi1721@users.noreply.github.com>
2021-10-16 18:39:03 +0530

    Update Crick_hackbio.sh

--- Crick_hackbio.sh
+++ Crick_hackbio.sh
@@ -11,14 +11,25 @@ list1=*.sh
 listsh=()
 
 
-#To avoid the bash script meant for csv creating being executedfor i in $list1;do	if [ $i != "einstein_Hackbio.sh" ]	then		chmod +x $i		listsh+=( $i )	fi;done touch einstein_Hackbio.csv
+#To avoid the bash script meant for csv creating being executed
 for i in $list1;
 do
-        if [ $i != "Team_Crick.sh" ]
+        if [ $i != "Crick_hackbio.sh" ]
         then
                  chmod +x $i
                  listsh+=( $i )
         fi;
 done
 
-touch Team_Crick.csv
+touch Crick_hackbio.csv
+
+#Executing files
+for f in *.py;do python "$f">>einstein_Hackbio.csv;done
+for f in *.c;do out1=${f:0:-2}
+		gcc $f -o $out1
+		./$out1 >>einstein_Hackbio.csv;done
+for f in $listsh;do ./"$f">>einstein_Hackbio.csv;done
+wget https://cran.r-project.org/src/contrib/DescTools_0.99.42.tar.gz
+R CMD INSTALL --no-lock DescTools_0.99.42.tar.gz
+for f in *.R;do Rscript "$f">>einstein_Hackbio.csv;done
+echo "Completed succesfully"
