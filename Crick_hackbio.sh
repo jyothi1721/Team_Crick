cbe55eb78db5e151cf9d15f2d4ebcdf5b2775830
Jyothi Guruprasad <88387089+jyothi1721@users.noreply.github.com>
2021-10-17 00:17:39 +0530

    Update Crick_hackbio.sh

--- Crick_hackbio.sh
+++ Crick_hackbio.sh
@@ -1,38 +1,18 @@
 #!/bin/bash
+REPO = https://github.com/jyothi1721/Team_Crick
+OUTPUT_FILE = Crick_hackbio.csv
 
-#Packages should be equal to or above the following: Python 3.8.10, BASH 5.0.17(1), R 3.6 (DescTools package installed)
+my_value=$(python Josphat.py)
+echo $my_value >> Crick_hackbio.csv
 
-Run the command: "$./Crick_hackbio.sh"
+my_value_1=$(python Nourhan.py)
+echo $my_value_1 > Crick_hackbio.csv
 
-#Downloading repo to local machine
-git clone https://github.com/jyothi1721/Team_Crick.git
-cd Team_Crick
-list1=*.sh
-listsh=()
+Rscript KV.R >> Crick_hackbio.csv
 
+Rscript Pratiksha.R >> Crick_hackbio.csv
 
-#To avoid the bash script meant for csv creating being executed
-for i in $list1;
-do
-        if [ $i != "Crick_hackbio.sh" ]
-        then
-                 chmod +x $i
-                 listsh+=( $i )
-        fi;
-done
+sed -i -e '1i"Name","Email","slack","biostack"' Crick_hackbio.csv
 
-touch Crick_hackbio.csv
+gcc Jyothi.C > Crick_hackbio.csv
 
-#Executing files
-for f in *.py;do py "$f">>Crick_hackbio.csv;done
-for f in *.C;do out1=${f:0:-2}
-		gcc $f -o $out1
-		./$out1 >>Crick_hackbio.csv;done
-for f in $listsh;do ./"$f">>Crick_hackbio.csv;done
-wget https://cran.r-project.org/src/contrib/DescTools_0.99.42.tar.gz
-R CMD INSTALL --no-lock DescTools_0.99.42.tar.gz
-for f in *.R;do Rscript "$f">>Crick_hackbio.csv;done
-for f in *.js;do node "$f">>Crick_hackbio.csv;done
-for f in *.C++;do clang -o $out1 $f
-                  ./$out1>>Crick_hackbio.csv
-echo "Completed succesfully"
